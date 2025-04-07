6e1dd81df5cef459feb49af26ec0d597c55d259e
Eesha <eechaudhary@s.sfusd.edu>
2025-04-07 13:01:06 -0700

    added more options in combining items for tree hollow

--- src/dungeons/tree_hollow.py
+++ src/dungeons/tree_hollow.py
@@ -3,10 +3,12 @@ from utils import *
 
 class TreeHollow:
     def __init__(self):
-        self.room_items = ["(s)hovel", "(b)erries"]
+        self.room_items = ["(sh)ovel", "(b)erries"]
         self.shovel_found = False
         self.combinator_dict = {
-            frozenset(["shovel", "sprout"]): "tree"
+            frozenset(["(sh)ovel", "sprout"]): "tree",
+            frozenset(["(sh)ovel", "(b)erries"]): "berry bush",
+            frozenset(["(b)erries", "sprout"]): "fresh salad"
         }
 
     def enter(self, game_key):
@@ -28,8 +30,8 @@ class TreeHollow:
 
             if choice == '1':
                 if not self.shovel_found:
-                    slow_print("You discover a rusty shovel among the roots.")
-                    self.room_items.append("shovel")
+                    slow_print("You discover a rusty (sh)ovel among the roots. You also spot (b)erries hanging on a nearby bush.")
+                    self.room_items.append("(s)hovel")
                     self.shovel_found = True
                 else:
                     slow_print("You already searched here.")
@@ -38,15 +40,16 @@ class TreeHollow:
                 if not self.room_items:
                     slow_print("Nothing to grab.")
                 else:
-                    slow_print("What would you like to grab?")
-                    for item in self.room_items:
-                        slow_print(f"- {item}")
+                    slow_print("What would you like to grab? (sh/b)")
                     item = input().lower()
-                    if item in self.room_items:
-                        self.room_items.remove(item)
-                        game_key.player.add_to_backpack(item)
+                    name_map = {"sh": "(sh)ovel", "b": "(b)erries"}
+                    name = name_map.get(item)
+                    if name and name in self.room_items:
+                        self.room_items.remove(name)
+                        game_key.player.add_to_backpack(name)
                     else:
                         slow_print("That item isn't here.")
+                  
 
             elif choice == '3':
                 game_key.player.print_backpack()
@@ -57,6 +60,10 @@ class TreeHollow:
                 i1 = input().lower()
                 slow_print("Combine with?")
                 i2 = input().lower()
+
+                name_map = {"sh": "(sh)ovel", "b": "(b)erries"}
+                i1 = name_map.get(i1, i1)
+                i2 = name_map.get(i2, i2)
                 key = frozenset([i1, i2])
                 if key in self.combinator_dict:
                     result = self.combinator_dict[key]
@@ -65,6 +72,20 @@ class TreeHollow:
                         game_key.player.remove_from_backpack(i2)
                         game_key.player.add_to_backpack(result)
                         slow_print("You created: " + result)
+
+
+                        if result == "sprout":
+                            slow_print("The clouds start to darken...")
+                            slow_print("Suddenly you are whisked away to tree hollow!")
+                            return "tree_hollow"
+                        elif result == "watery seeds":
+                            slow_print("You wasted your vital resources.")
+                            slow_print("You can no longer escape.")
+                        elif result == "mud":
+                            slow_print("You wasted your vital resources.")
+                            slow_print("You can no longer escape.")
+                            #game_key.game_over = True
+                            #return None
                     else:
                         slow_print("You're missing something.")
                 else:
@@ -83,12 +104,6 @@ class TreeHollow:
                         pause(2.5)
                     else:
                         slow_print("You can't use that item.")
-
-
-
-
-
-
 
 
 
